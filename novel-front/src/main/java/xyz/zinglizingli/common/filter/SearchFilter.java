7bf7db85e992e12e0c75126f3bef6909d39d1228
xiongxiaoyang <773861846@qq.com>
2019-11-28 15:43:22 +0800

    漫画图片防封功能增强

--- novel-front/src/main/java/xyz/zinglizingli/common/filter/SearchFilter.java
+++ novel-front/src/main/java/xyz/zinglizingli/common/filter/SearchFilter.java
@@ -3,6 +3,7 @@ package xyz.zinglizingli.common.filter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -15,7 +16,7 @@ import xyz.zinglizingli.common.utils.SpringUtil;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
 import java.net.URLDecoder;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -105,23 +106,23 @@ public class SearchFilter implements Filter {
             final String method = req.getMethod();
 
 
-                // 案例：充当客户端通过restTemplate请求网络数据，并充当服务端将数据返回给浏览器
-                // 客户端请求数据：输入流（byte[]）==》字符串
-                // 服务端响应数据：字符串 == 》 输出流（byte[]）
+            // 案例：充当客户端通过restTemplate请求网络数据，并充当服务端将数据返回给浏览器
+            // 客户端请求数据：输入流（byte[]）==》字符串
+            // 服务端响应数据：字符串 == 》 输出流（byte[]）
 
-                //默认方式：
-                //RestTemplate restTemplate = new RestTemplate();
-                // ①当返回的response-header的content-type属性有charset值时，
-                // restTemplate的StringHttpMessageConverter会设置默认charset为content-type属性
-                // charset值
-                // StringHttpMessageConverter.setDefaultCharset(Charset.forName(charset));
-                // ②当返回的response-header的content-type属性没有charset值时
-                // restTemplate的StringHttpMessageConverter会使用默认的charset即ISO-8859-1
+            //默认方式：
+            //RestTemplate restTemplate = new RestTemplate();
+            // ①当返回的response-header的content-type属性有charset值时，
+            // restTemplate的StringHttpMessageConverter会设置默认charset为content-type属性
+            // charset值
+            // StringHttpMessageConverter.setDefaultCharset(Charset.forName(charset));
+            // ②当返回的response-header的content-type属性没有charset值时
+            // restTemplate的StringHttpMessageConverter会使用默认的charset即ISO-8859-1
 
-                    // 对服务端请求返回的输入流（byte[]）采用何种编码转换成字符串（String）
-                    restTemplate = RestTemplateUtil.getInstance("utf-8");//请求html/css/js等文件
-                    // 对客户端响应返回的字符串（String）采用何种编码转换成输出流（byte[]）
-                    resp.setCharacterEncoding("utf-8");
+            // 对服务端请求返回的输入流（byte[]）采用何种编码转换成字符串（String）
+            restTemplate = RestTemplateUtil.getInstance("utf-8");//请求html/css/js等文件
+            // 对客户端响应返回的字符串（String）采用何种编码转换成输出流（byte[]）
+            resp.setCharacterEncoding("utf-8");
 
                         /*//=====现在浏览器有编码自动识别功能，所以上面的代码没有加content-type的Header也没有问题==========
                         //=====正确做法应该是下面代码片段1和代码片段2二选一==========
@@ -142,45 +143,81 @@ public class SearchFilter implements Filter {
 */
 
 
-                if (HttpMethod.GET.name().equals(method)) {
+            if (HttpMethod.GET.name().equals(method)) {
 
 
-                    String fileName = requestURI.substring(requestURI.lastIndexOf("/") + 1);
-                    if (localFileFix.contains(fileName) || fileName.startsWith("9a4a540e-1759-4268-90fa-7fb652c3604a.")) {
-                        filterChain.doFilter(servletRequest, servletResponse);
+                String fileName = requestURI.substring(requestURI.lastIndexOf("/") + 1);
+                if (localFileFix.contains(fileName) || fileName.startsWith("9a4a540e-1759-4268-90fa-7fb652c3604a.")) {
+                    filterChain.doFilter(servletRequest, servletResponse);
+                    return;
+                }
+
+
+                String queryString = req.getQueryString();
+                if (queryString != null && queryString.length() > 0 && !queryString.contains("bsh_bid=")) {
+                    queryString = "?" + URLDecoder.decode(req.getQueryString());
+                } else {
+                    queryString = "";
+                }
+
+
+                if (forObject == null) {
+                    String postFix = requestURI.substring(requestURI.lastIndexOf(".") + 1);
+                    if (picPostFix.contains(postFix)) {
+                        // 对服务端请求返回的输入流（byte[]）采用何种编码转换成字符串（String）
+                        restTemplate = RestTemplateUtil.getInstance("iso-8859-1");//请求图片
+                        resp.setContentType("image/apng");
+                    }
+                    if (requestURI.startsWith("/manhua/images/")) {
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.add("Referer","https://www.dmzj.com");
+                        HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
+                        String realUrl = "https://images.dmzj.com/" + requestURI.substring(15);
+                        ResponseEntity<Resource> resEntity = restTemplate.exchange(realUrl, HttpMethod.GET, requestEntity, Resource.class);
+                        InputStream input = resEntity.getBody().getInputStream();
+                        OutputStream out = resp.getOutputStream();
+                        byte[] b = new byte[4096];
+                        for (int n; (n = input.read(b)) != -1;) {
+                            out.write(b, 0, n);
+                        }
+                        input.close();
+                        out.close();
                         return;
-                    }
+
+
+                        // 对服务端请求返回的输入流（byte[]）采用何种编码转换成字符串（String）
 
 
 
-                    String queryString = req.getQueryString();
-                    if (queryString != null && queryString.length() > 0 && !queryString.contains("bsh_bid=")) {
-                        queryString = "?" + URLDecoder.decode(req.getQueryString());
-                    } else {
-                        queryString = "";
-                    }
 
+                    } else if(requestURI.startsWith("/manhua/statics/")){
+                        String realPath = "https://static.dmzj.com/"+requestURI.substring(16);
+                        ResponseEntity<String> forEntity = restTemplate.getForEntity(realPath, String.class);
+                        forObject = forEntity.getBody();
+                        forObject = forObject.replaceAll("https://images.dmzj.com/", "/manhua/images/");
 
-                    if (forObject == null) {
+                    }else {
+                        String realPath = "https://www.dmzj.com/" + requestURI.substring(8);
 
-
-                        ResponseEntity<String> forEntity = restTemplate.getForEntity("https://www.dmzj.com/"+requestURI.substring(8), String.class);
+                        ResponseEntity<String> forEntity = restTemplate.getForEntity(realPath, String.class);
                         forObject = forEntity.getBody();
 
                         //  forObject = new String(forObject.getBytes("ISO-8859-1"),"utf-8");
-                      forObject=forObject.replace("https://www.dmzj.com/js/ad/ad_12.js","");
-                      forObject=forObject.replace("https://www.dmzj.com/js/ad/ad_13.js","");
-                        forObject=forObject.replace("https://static.dmzj.com/ocomic/js/dmzjMhFinally-new.js","");
-                        forObject=forObject.replace("https://static.dmzj.com/ocomic/js/dmzjMhFinally-new.js","");
-                        forObject=forObject.replace("https://static.dmzj.com/module/js/float_code.js","");
-                        forObject=forObject.replaceAll("<script type=\"text/javascript\">var cnzz_protocol =[^<]+</script>","");
-                        forObject=forObject.replaceAll("https://static.dmzj.com/public/js/dmzj-land-2015.6.js","");
-                        forObject=forObject.replaceAll("<script type=\"text/javascript\">var cnzz_protocol =[^<]+</script>","");
-                        forObject=forObject.replaceAll("<script type=\"text/javascript\">var cnzz_protocol =[^<]+</script>","");
-                        forObject=forObject.replaceAll("<script type=\"text/javascript\">var cnzz_protocol =[^<]+</script>","");
-                        forObject=forObject.replaceAll("globalNav.js","");
-                        forObject=forObject.replaceAll("TSB.js","");
-                        forObject=forObject+"<script>var browser={\n" +
+                        forObject = forObject.replace("https://www.dmzj.com/js/ad/ad_12.js", "");
+                        forObject = forObject.replace("https://www.dmzj.com/js/ad/ad_13.js", "");
+                        forObject = forObject.replace("https://static.dmzj.com/ocomic/js/dmzjMhFinally-new.js", "");
+                        forObject = forObject.replace("https://static.dmzj.com/ocomic/js/dmzjMhFinally-new.js", "");
+                        forObject = forObject.replace("https://static.dmzj.com/module/js/float_code.js", "");
+                        forObject = forObject.replaceAll("<script type=\"text/javascript\">var cnzz_protocol =[^<]+</script>", "");
+                        forObject = forObject.replaceAll("https://static.dmzj.com/public/js/dmzj-land-2015.6.js", "");
+                        forObject = forObject.replaceAll("<script type=\"text/javascript\">var cnzz_protocol =[^<]+</script>", "");
+                        forObject = forObject.replaceAll("<script type=\"text/javascript\">var cnzz_protocol =[^<]+</script>", "");
+                        forObject = forObject.replaceAll("<script type=\"text/javascript\">var cnzz_protocol =[^<]+</script>", "");
+                        forObject = forObject.replaceAll("globalNav.js", "");
+                        forObject = forObject.replaceAll("TSB.js", "");
+                        forObject = forObject.replaceAll("https://images.dmzj.com/", "/manhua/images/");
+                        forObject = forObject.replaceAll("https://static.dmzj.com/", "/manhua/statics/");
+                        forObject = forObject + "<script>var browser={\n" +
                                 "    versions:function(){\n" +
                                 "    var u = window.navigator.userAgent;\n" +
                                 "    return {\n" +
@@ -199,7 +236,7 @@ public class SearchFilter implements Filter {
                                 "    }()\n" +
                                 "};" +
                                 "</script>";
-                        forObject=forObject+"<script>$(function(){$(\"#app_manhua\").remove();" +
+                        forObject = forObject + "<script>$(function(){$(\"#app_manhua\").remove();" +
                                 "$('.btmBtnBox').remove();$('.red_box').remove()" +
                                 ";$('.mainNav').remove();$('.wrap_last_head').remove();$('.wrap_last_mid').remove()" +
                                 ";$('.comic_gd').remove();$('.comic_last').remove();$('.side_bar').remove()" +
@@ -239,7 +276,7 @@ public class SearchFilter implements Filter {
 //                                            "})();\n" +
 //                                            "</script>\n</head>")//页面访问自动推送到百度
 //                                    .replaceAll("<script.*wap\\.js.*script>", "")//去除广告
-                            ;
+                        ;
 
 
 //                            if (requestURI.matches(SUANWEI_BOOK_HTML_REGEX)) {
@@ -276,46 +313,46 @@ public class SearchFilter implements Filter {
 //
 //                            }
                     }
-
-
-                } else {
-
-
-                    Map<String, String[]> oldParameterMap = req.getParameterMap();
-                    Map<String, String> newParameterMap = new HashMap<>();
-                    Set<Map.Entry<String, String[]>> entries = oldParameterMap.entrySet();
-                    for (Map.Entry<String, String[]> entry : entries) {
-                        newParameterMap.put(entry.getKey(), entry.getValue()[0]);
-                    }
-
-                    MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-                    map.setAll(newParameterMap);
-                    HttpHeaders headers = new HttpHeaders();
-                    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-                    HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-                    forObject = restTemplate.postForEntity("https://www.dmzj.com/"+requestURI.substring(8), request, String.class).getBody();
-                    // forObject = new String(forObject.getBytes("ISO-8859-1"),"utf-8");
-                    forObject = forObject.replaceAll("/manhua/", "https://www.dmzj.com/")
-                            .replaceAll("笔趣岛", "酸味书屋")
-                            .replaceAll("笔趣阁", "酸味书屋")
-                            .replaceFirst("</head>", "<script>\n" +
-                                    "var _hmt = _hmt || [];" +
-                                    "(function() {" +
-                                    "  var hm = document.createElement(\"script\");" +
-                                    "  hm.src = \"https://hm.baidu.com/hm.js?0bd7345ca6b694ea3dfbe87da008082e\";" +
-                                    "  var s = document.getElementsByTagName(\"script\")[0]; " +
-                                    "  s.parentNode.insertBefore(hm, s);" +
-                                    "})();" +
-                                    "</script></head>")
-                            .replaceAll("<a\\s+href=\"/bookcase.php\">书架</a>", "<a href=\"" + noteURI.get(new Random().nextInt(noteURI.size())) + "\">笔记</a>")
-                            .replaceAll("<a href=\"/.*/\">返回</a>", "<a href=\"javascript:history.go(-1)\">返回</a>")
-                    ;
-                    forObject = setBookURIToHTML(forObject, SUANWEI_BOOK_REGEX);
-                    //resp.setCharacterEncoding("utf-8");
-                    //setContentType(postFix, resp);
-
                 }
 
+
+            } else {
+
+
+                Map<String, String[]> oldParameterMap = req.getParameterMap();
+                Map<String, String> newParameterMap = new HashMap<>();
+                Set<Map.Entry<String, String[]>> entries = oldParameterMap.entrySet();
+                for (Map.Entry<String, String[]> entry : entries) {
+                    newParameterMap.put(entry.getKey(), entry.getValue()[0]);
+                }
+
+                MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+                map.setAll(newParameterMap);
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+                forObject = restTemplate.postForEntity("https://www.dmzj.com/" + requestURI.substring(8), request, String.class).getBody();
+                // forObject = new String(forObject.getBytes("ISO-8859-1"),"utf-8");
+                forObject = forObject.replaceAll("/manhua/", "https://www.dmzj.com/")
+                        .replaceAll("笔趣岛", "酸味书屋")
+                        .replaceAll("笔趣阁", "酸味书屋")
+                        .replaceFirst("</head>", "<script>\n" +
+                                "var _hmt = _hmt || [];" +
+                                "(function() {" +
+                                "  var hm = document.createElement(\"script\");" +
+                                "  hm.src = \"https://hm.baidu.com/hm.js?0bd7345ca6b694ea3dfbe87da008082e\";" +
+                                "  var s = document.getElementsByTagName(\"script\")[0]; " +
+                                "  s.parentNode.insertBefore(hm, s);" +
+                                "})();" +
+                                "</script></head>")
+                        .replaceAll("<a\\s+href=\"/bookcase.php\">书架</a>", "<a href=\"" + noteURI.get(new Random().nextInt(noteURI.size())) + "\">笔记</a>")
+                        .replaceAll("<a href=\"/.*/\">返回</a>", "<a href=\"javascript:history.go(-1)\">返回</a>")
+                ;
+                forObject = setBookURIToHTML(forObject, SUANWEI_BOOK_REGEX);
+                //resp.setCharacterEncoding("utf-8");
+                //setContentType(postFix, resp);
+
+            }
 
 
         } catch (RuntimeException e) {
@@ -333,6 +370,7 @@ public class SearchFilter implements Filter {
             //resp.setCharacterEncoding("utf-8");
 
         }
+
         resp.getWriter().print(forObject);
         return;
     }
