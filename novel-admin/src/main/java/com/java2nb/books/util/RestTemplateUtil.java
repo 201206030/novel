de2bb731b586d5b68073d9f0723e3fe85cfb802f
xiongxiaoyang <773861846@qq.com>
2019-12-12 18:00:06 +0800

    小说爬虫请求忽略SSL证书

--- novel-admin/src/main/java/com/java2nb/books/util/RestTemplateUtil.java
+++ novel-admin/src/main/java/com/java2nb/books/util/RestTemplateUtil.java
@@ -1,22 +1,46 @@
 package com.java2nb.books.util;
 
+import lombok.SneakyThrows;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
 import java.nio.charset.Charset;
+import java.security.cert.X509Certificate;
 import java.util.List;
 
 public class RestTemplateUtil {
 
 
+    @SneakyThrows
     public static RestTemplate getInstance(String charset) {
-        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        httpRequestFactory.setConnectionRequestTimeout(3000);
-        httpRequestFactory.setConnectTimeout(3000);
-        httpRequestFactory.setReadTimeout(10000);
-        RestTemplate restTemplate = new RestTemplate(httpRequestFactory);
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        //忽略证书
+        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpClient);
+        requestFactory.setConnectionRequestTimeout(3000);
+        requestFactory.setConnectTimeout(3000);
+        requestFactory.setReadTimeout(10000);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
         List<HttpMessageConverter<?>> list = restTemplate.getMessageConverters();
         for (HttpMessageConverter<?> httpMessageConverter : list) {
             if(httpMessageConverter instanceof StringHttpMessageConverter) {
