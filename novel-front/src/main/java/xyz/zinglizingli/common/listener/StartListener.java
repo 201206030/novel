72d733b8fbcae3dc2ee444021255042b0d809fa5
xxy <1179705413@qq.com>
2019-12-12 21:16:58 +0800

    fixed bug

--- novel-front/src/main/java/xyz/zinglizingli/common/listener/StartListener.java
+++ novel-front/src/main/java/xyz/zinglizingli/common/listener/StartListener.java
@@ -1,11 +1,17 @@
 package xyz.zinglizingli.common.listener;
 
+import com.sun.javafx.tk.Toolkit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Component;
 import xyz.zinglizingli.common.crawl.BaseCrawlSource;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Component
 @Slf4j
@@ -17,19 +23,18 @@ public class StartListener implements ApplicationListener<ContextRefreshedEvent>
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         log.info("程序启动");
+        crawlBook();
+    }
 
-
-        while (true) {
-            try {
+    private void crawlBook() {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
                 log.debug("crawlBooks执行中。。。。。。。。。。。。");
                 crawlSource.parse();
-                Thread.sleep(1000 * 60 * 5);
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
+                crawlBook();
             }
-
-        }
-
+        },1000*60*5);
     }
 
 }
