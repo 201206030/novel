f81a318421edae5e16a7d13d028ba3f7ac563b98
xxy <1179705413@qq.com>
2019-12-26 07:44:13 +0800

    增加本地图片缓存

--- novel-front/src/main/java/xyz/zinglizingli/books/core/filter/BookFilter.java
+++ novel-front/src/main/java/xyz/zinglizingli/books/core/filter/BookFilter.java
@@ -61,6 +61,7 @@ public class BookFilter implements Filter {
 
             //本地图片
             if (requestUri.contains(Constants.LOCAL_PIC_PREFIX)) {
+                resp.setDateHeader("expires", System.currentTimeMillis()+60*60*24*10*1000);//缓存10天
                 OutputStream out = resp.getOutputStream();
                 InputStream input = new FileInputStream(new File(picSavePath + requestUri));
                 byte[] b = new byte[4096];
