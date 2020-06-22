05d9e22df941fa6f4fe78569b8363dd0e67191c9
xiongxiaoyang <773861846@qq.com>
2020-06-22 16:07:41 +0800

    2.6.0发布，新增笔趣窝源

---
+++ novel-front/src/main/java/xyz/zinglizingli/books/core/config/CrawlBiquwoConfig.java
@@ -0,0 +1,27 @@
+package xyz.zinglizingli.books.core.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import xyz.zinglizingli.books.core.crawl.BaseHtmlCrawlSource;
+import xyz.zinglizingli.books.core.crawl.BiquCrawlSource;
+
+/**
+ * @author 11797
+ */
+@Slf4j
+@Configuration
+public class CrawlBiquwoConfig {
+
+
+    @Bean
+    @ConfigurationProperties(prefix = "biquwo.crawlsource") // prefix值必须是application.yml中对应属性的前缀
+    @ConditionalOnProperty(prefix = "crawl.website",name = "type",havingValue = "6")
+    public BaseHtmlCrawlSource biquwoCrawlSource() {
+        return new BiquCrawlSource();
+    }
+
+
+}
