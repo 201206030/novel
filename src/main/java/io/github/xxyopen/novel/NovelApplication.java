a6cf0a7010589d2d5511197933a37e7bb90c4d69
xiongxiaoyang <773861846@qq.com>
2022-05-11 20:01:49 +0800

    feat: 集成 mybatis-plus 和 mybatis-plus 代码生成器

--- src/main/java/io/github/xxyopen/novel/NovelApplication.java
+++ src/main/java/io/github/xxyopen/novel/NovelApplication.java
@@ -1,9 +1,11 @@
 package io.github.xxyopen.novel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("io.github.xxyopen.novel.dao.mapper")
 public class NovelApplication {
 
 	public static void main(String[] args) {
