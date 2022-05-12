e10931f4de195cf14bc03623d9bd471b3ed5603f
xiongxiaoyang <773861846@qq.com>
2022-05-12 11:35:26 +0800

    feat(NovelApplication.java): 开启缓存

--- src/main/java/io/github/xxyopen/novel/NovelApplication.java
+++ src/main/java/io/github/xxyopen/novel/NovelApplication.java
@@ -1,15 +1,38 @@
 package io.github.xxyopen.novel;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 @SpringBootApplication
 @MapperScan("io.github.xxyopen.novel.dao.mapper")
+@EnableCaching
+@Slf4j
 public class NovelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovelApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context){
+		return args -> {
+			Map<String, CacheManager> beans = context.getBeansOfType(CacheManager.class);
+			log.info("加载了如下缓存管理器：");
+			beans.forEach((k,v)->{
+				log.info("{}:{}",k,v.getClass().getName());
+				log.info("缓存：{}",v.getCacheNames());
+			});
+
+		};
 	}
 
 }
