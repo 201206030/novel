3092635141635e9c9eef3552be813973c83bbc69
xiongxiaoyang <773861846@qq.com>
2022-05-17 05:36:02 +0800

    build: 集成 JWT 支持

--- src/main/java/io/github/xxyopen/novel/core/constant/SystemConfigConsts.java
+++ src/main/java/io/github/xxyopen/novel/core/constant/SystemConfigConsts.java
@@ -13,6 +13,21 @@ public class SystemConfigConsts {
     }
 
     /**
+     * 前台门户系统标识
+     * */
+    public static final String NOVEL_FRONT_KEY = "front";
+
+    /**
+     * 作家管理系统标识
+     * */
+    public static final String NOVEL_AUTHOR_KEY = "author";
+
+    /**
+     * 后台管理系统标识
+     * */
+    public static final String NOVEL_ADMIN_KEY = "admin";
+
+    /**
      * 小说前台门户系统域
      * */
     public static final String NOVEL_FRONT_WEB_ORIGIN = "http://localhost:1024";
---
+++ src/main/java/io/github/xxyopen/novel/core/util/JwtUtils.java
@@ -0,0 +1,77 @@
+package io.github.xxyopen.novel.core.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+/**
+ * JWT 工具类
+ *
+ * @author xiongxiaoyang
+ * @date 2022/5/17
+ */
+@ConditionalOnProperty("jwt.secret")
+@Component
+@Slf4j
+public class JwtUtils {
+
+    /**
+     * 注入JWT加密密钥
+     */
+    @Value("${jwt.secret}")
+    private String secret;
+
+    /**
+     * 定义系统标识头常量
+     */
+    private static final String HEADER_SYSTEM_KEY = "systemKeyHeader";
+
+    /**
+     * 根据用户ID生成JWT
+     * @param uid 用户ID
+     * @param systemKey 系统标识
+     * @return JWT
+     */
+    public String generateToken(Long uid, String systemKey) {
+        return Jwts.builder()
+                .setHeaderParam(HEADER_SYSTEM_KEY, systemKey)
+                .setSubject(uid.toString())
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)))
+                .compact();
+    }
+
+    /**
+     * 解析JWT返回用户ID
+     * @param token JWT
+     * @param systemKey 系统标识
+     * @return 用户ID
+     */
+    public Long parseToken(String token, String systemKey) {
+        Jws<Claims> claimsJws;
+        try {
+            claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(token);
+            // OK, we can trust this JWT
+            // 判断该 JWT 是否属于指定系统
+            if (Objects.equals(claimsJws.getHeader().get(HEADER_SYSTEM_KEY), systemKey)) {
+                return Long.parseLong(claimsJws.getBody().getSubject());
+            }
+        } catch (JwtException e) {
+            log.warn("JWT解析失败:{}", token);
+            // don't trust the JWT!
+        }
+        return null;
+    }
+
+}
