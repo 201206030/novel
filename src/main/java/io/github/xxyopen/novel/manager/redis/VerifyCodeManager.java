520faff51f61aa0543c4e839349dffc241500c75
xiongxiaoyang <773861846@qq.com>
2022-06-02 12:15:21 +0800

    docs: 注释

--- src/main/java/io/github/xxyopen/novel/manager/redis/VerifyCodeManager.java
+++ src/main/java/io/github/xxyopen/novel/manager/redis/VerifyCodeManager.java
@@ -25,7 +25,7 @@ public class VerifyCodeManager {
     private final StringRedisTemplate stringRedisTemplate;
 
     /**
-     * 生成图片验证码，并放入缓存中
+     * 生成图形验证码，并放入 Redis 中
      */
     public String genImgVerifyCode(String sessionId) throws IOException {
         String verifyCode = ImgVerifyCodeUtils.getRandomVerifyCode(4);
@@ -36,14 +36,14 @@ public class VerifyCodeManager {
     }
 
     /**
-     * 校验图片验证码
+     * 校验图形验证码
      */
     public boolean imgVerifyCodeOk(String sessionId, String verifyCode) {
         return Objects.equals(stringRedisTemplate.opsForValue().get(CacheConsts.IMG_VERIFY_CODE_CACHE_KEY + sessionId), verifyCode);
     }
 
     /**
-     * 删除验证码
+     * 从 Redis 中删除验证码
      */
     public void removeImgVerifyCode(String sessionId) {
         stringRedisTemplate.delete(CacheConsts.IMG_VERIFY_CODE_CACHE_KEY + sessionId);
