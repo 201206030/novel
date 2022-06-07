925da99d0b9cdca934795dd3404be6073b56ad8d
xiongxiaoyang <773861846@qq.com>
2022-06-07 18:17:08 +0800

    perf: Actuator Endpoints 保护

--- src/main/java/io/github/xxyopen/novel/NovelApplication.java
+++ src/main/java/io/github/xxyopen/novel/NovelApplication.java
@@ -4,12 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 
 import java.util.Map;
 
@@ -20,21 +24,30 @@ import java.util.Map;
 @Slf4j
 public class NovelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NovelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NovelApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext context){
-		return args -> {
-			Map<String, CacheManager> beans = context.getBeansOfType(CacheManager.class);
-			log.info("加载了如下缓存管理器：");
-			beans.forEach((k,v)->{
-				log.info("{}:{}",k,v.getClass().getName());
-				log.info("缓存：{}",v.getCacheNames());
-			});
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+            Map<String, CacheManager> beans = context.getBeansOfType(CacheManager.class);
+            log.info("加载了如下缓存管理器：");
+            beans.forEach((k, v) -> {
+                log.info("{}:{}", k, v.getClass().getName());
+                log.info("缓存：{}", v.getCacheNames());
+            });
 
-		};
-	}
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .requestMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeRequests(requests -> requests.anyRequest().hasRole("ENDPOINT_ADMIN"));
+        http.httpBasic();
+        return http.build();
+    }
 
 }
