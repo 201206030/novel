5852cc6d260b03a40374250d63b23b1fdb42b9db
xiongxiaoyang <773861846@qq.com>
2022-07-01 06:30:11 +0800

    style: 代码格式化

--- src/main/java/io/github/xxyopen/novel/manager/cache/BookInfoCacheManager.java
+++ src/main/java/io/github/xxyopen/novel/manager/cache/BookInfoCacheManager.java
@@ -32,8 +32,7 @@ public class BookInfoCacheManager {
     /**
      * 从缓存中查询小说信息（先判断缓存中是否已存在，存在则直接从缓存中取，否则执行方法体中的逻辑后缓存结果）
      */
-    @Cacheable(
-        cacheManager = CacheConsts.CAFFEINE_CACHE_MANAGER,
+    @Cacheable(cacheManager = CacheConsts.CAFFEINE_CACHE_MANAGER,
         value = CacheConsts.BOOK_INFO_CACHE_NAME)
     public BookInfoRespDto getBookInfo(Long id) {
         return cachePutBookInfo(id);
@@ -42,8 +41,7 @@ public class BookInfoCacheManager {
     /**
      * 缓存小说信息（不管缓存中是否存在都执行方法体中的逻辑，然后缓存起来）
      */
-    @CachePut(
-        cacheManager = CacheConsts.CAFFEINE_CACHE_MANAGER,
+    @CachePut(cacheManager = CacheConsts.CAFFEINE_CACHE_MANAGER,
         value = CacheConsts.BOOK_INFO_CACHE_NAME)
     public BookInfoRespDto cachePutBookInfo(Long id) {
         // 查询基础信息
@@ -74,8 +72,7 @@ public class BookInfoCacheManager {
             .build();
     }
 
-    @CacheEvict(
-        cacheManager = CacheConsts.CAFFEINE_CACHE_MANAGER,
+    @CacheEvict(cacheManager = CacheConsts.CAFFEINE_CACHE_MANAGER,
         value = CacheConsts.BOOK_INFO_CACHE_NAME)
     public void evictBookInfoCache(Long ignoredId) {
         // 调用此方法自动清除小说信息的缓存
@@ -84,16 +81,15 @@ public class BookInfoCacheManager {
     /**
      * 查询每个类别下最新更新的 500 个小说ID列表，并放入缓存中 1 个小时
      */
-    @Cacheable(
-        cacheManager = CacheConsts.CAFFEINE_CACHE_MANAGER,
+    @Cacheable(cacheManager = CacheConsts.CAFFEINE_CACHE_MANAGER,
         value = CacheConsts.LAST_UPDATE_BOOK_ID_LIST_CACHE_NAME)
     public List<Long> getLastUpdateIdList(Long categoryId) {
         QueryWrapper<BookInfo> queryWrapper = new QueryWrapper<>();
-        queryWrapper
-            .eq(DatabaseConsts.BookTable.COLUMN_CATEGORY_ID, categoryId)
+        queryWrapper.eq(DatabaseConsts.BookTable.COLUMN_CATEGORY_ID, categoryId)
             .gt(DatabaseConsts.BookTable.COLUMN_WORD_COUNT, 0)
             .orderByDesc(DatabaseConsts.BookTable.COLUMN_LAST_CHAPTER_UPDATE_TIME)
             .last(DatabaseConsts.SqlEnum.LIMIT_500.getSql());
         return bookInfoMapper.selectList(queryWrapper).stream().map(BookInfo::getId).toList();
     }
+
 }
