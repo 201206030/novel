a3a2384c95a94b2cd3a9d4ca97b97d776fea179e
xiongxiaoyang <1179705413@qq.com>
2022-09-21 16:57:14 +0800

    build: 升级 Spring Boot 版本

--- src/main/java/io/github/xxyopen/novel/core/config/EsConfig.java
+++ src/main/java/io/github/xxyopen/novel/core/config/EsConfig.java
@@ -1,12 +1,6 @@
 package io.github.xxyopen.novel.core.config;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
-import lombok.RequiredArgsConstructor;
-import org.elasticsearch.client.RestClient;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,19 +11,14 @@ import org.springframework.context.annotation.Configuration;
  * @date 2022/5/23
  */
 @Configuration
-@ConditionalOnProperty(prefix = "spring.elasticsearch", name = "enabled", havingValue = "true")
-@RequiredArgsConstructor
 public class EsConfig {
 
+    /**
+     * 解决 ElasticsearchClientConfigurations 修改默认 ObjectMapper 配置的问题
+     */
     @Bean
-    public ElasticsearchClient elasticsearchClient(RestClient restClient) {
-
-        // Create the transport with a Jackson mapper
-        ElasticsearchTransport transport = new RestClientTransport(
-            restClient, new JacksonJsonpMapper());
-
-        // And create the API client
-        return new ElasticsearchClient(transport);
+    JacksonJsonpMapper jacksonJsonpMapper() {
+        return new JacksonJsonpMapper();
     }
 
 }
