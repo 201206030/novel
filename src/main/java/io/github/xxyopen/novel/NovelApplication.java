cd3a7206a99e482d43a1c61ddc4bc2c849845dab
xiongxiaoyang <1179705413@qq.com>
2025-03-19 09:58:04 +0800

    perf: instanceof 智能转型

--- src/main/java/io/github/xxyopen/novel/NovelApplication.java
+++ src/main/java/io/github/xxyopen/novel/NovelApplication.java
@@ -39,11 +39,10 @@ public class NovelApplication {
                 log.info("{}:{}", k, v.getClass().getName());
                 log.info("缓存：{}", v.getCacheNames());
             });
-            if(dataSource instanceof HikariDataSource) {
+            if(dataSource instanceof HikariDataSource hikariDataSource) {
                 // 如果使用的是HikariDataSource，需要提前创建连接池，而不是在第一次访问数据库时才创建，提高第一次访问接口的速度
                 log.info("创建连接池...");
                 try (Connection connection = dataSource.getConnection()) {
-                    HikariDataSource hikariDataSource = (HikariDataSource) dataSource;
                     log.info("最小空闲连接数：{}", hikariDataSource.getMinimumIdle());
                     log.info("最大连接数：{}", hikariDataSource.getMaximumPoolSize());
                     log.info("创建连接池完成.");
